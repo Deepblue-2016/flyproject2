1fdceca2d769b9d3947dd2f52ef629b18ea8758b
Administrator <sunxin@neusoft.com>
2019-04-01 17:02:31 +0800

    完成models的编写，创建了所有数据表

--- jie/models.py
+++ jie/models.py
@@ -1,3 +1,55 @@
 from django.db import models
 
+
 # Create your models here.
+class TopicInfo(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.CharField(max_length=300)
+    is_delete = models.IntegerField()
+    view_times = models.IntegerField()
+    kiss_num = models.IntegerField()
+    is_top = models.IntegerField()
+    is_good = models.IntegerField()
+    is_over = models.IntegerField()
+    comment_num = models.IntegerField()
+    create_time = models.DateTimeField()
+    user = models.ForeignKey('user.UserInfo', on_delete=models.CASCADE)
+    category = models.ManyToManyField('CategoryInfo', through='TopicCategory')
+
+    class Meta:
+        db_table = 'topicinfo'
+
+
+class CategoryInfo(models.Model):
+    typename = models.CharField(max_length=10)
+
+    class Meta:
+        db_table = 'categoryinfo'
+
+
+class TopicCategory(models.Model):
+    topic = models.ForeignKey('TopicInfo', on_delete=models.CASCADE)
+    category = models.ForeignKey('CategoryInfo', on_delete=models.CASCADE)
+
+    class Meta:
+        db_table = 'topic_category'
+
+
+class Comment(models.Model):
+    user = models.ForeignKey('user.UserInfo', on_delete=models.CASCADE)
+    topic = models.ForeignKey('TopicInfo', on_delete=models.CASCADE)
+    comment_content = models.CharField(max_length=300)
+    zan_num = models.IntegerField()
+    is_accept = models.IntegerField()
+    comment_time = models.DateTimeField()
+
+    class Meta:
+        db_table = 'comment'
+
+
+class CommentAgree(models.Model):
+    user = models.ForeignKey('user.UserInfo', on_delete=models.CASCADE)
+    comment = models.ForeignKey('Comment', on_delete=models.CASCADE)
+
+    class Meta:
+        db_table = 'comment_agree'
--- user/models.py
+++ user/models.py
@@ -1,3 +1,45 @@
 from django.db import models
 
+
 # Create your models here.
+class UserInfo(models.Model):
+    email = models.CharField(max_length=25)
+    nickname = models.CharField(max_length=15)
+    passwd = models.CharField(max_length=16)
+    city = models.CharField(max_length=20)
+    sign = models.CharField(max_length=50)
+    gender = models.IntegerField()
+    photo = models.ImageField(upload_to='photo')
+    qq = models.IntegerField()
+    weibo = models.CharField(max_length=20)
+    join_time = models.DateTimeField()
+    kiss_num = models.IntegerField()
+    vip_grade = models.IntegerField()
+    is_bigv = models.IntegerField()
+    is_active = models.IntegerField()
+
+    class Meta:
+        db_table = 'userinfo'
+
+
+class QiandaoInfo(models.Model):
+    user = models.ForeignKey('UserInfo', on_delete=models.CASCADE)
+    total = models.IntegerField()
+    create_time = models.DateTimeField()
+
+    class Meta:
+        db_table = 'qiandaoinfo'
+
+
+class MessageInfo(models.Model):
+    content = models.CharField(max_length=100)
+    send_user = models.ForeignKey('UserInfo', related_name='send_user_id', on_delete=models.CASCADE)
+    recv_user = models.ForeignKey('UserInfo', related_name='recv_user_id', on_delete=models.CASCADE)
+    topic = models.ForeignKey('jie.TopicInfo', on_delete=models.CASCADE)
+    msg_type = models.IntegerField()
+    is_read = models.IntegerField()
+    create_time = models.DateTimeField()
+    is_delete = models.IntegerField()
+
+    class Meta:
+        db_table = 'messageinfo'
